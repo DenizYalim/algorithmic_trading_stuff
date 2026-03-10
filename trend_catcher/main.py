c09e6773b01e7f6bdd318f100ca260631009028b
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-03-11 00:32:09 +0100

    restructured

--- trend_catcher/main.py
+++ trend_catcher/main.py
@@ -2,6 +2,7 @@ from market_news_ABS import MarketNew
 from broker_ABS import Broker, SimulatedBroker
 from trader_ABS import RegexTrader
 
+
 def ask_for_news() -> MarketNew:
     # Implement logic to fetch news from various sources and return a MarketNew object
     return "you asked for news and you got it (placeholder)"
@@ -10,11 +11,11 @@ def ask_for_news() -> MarketNew:
 def start():  # REGEX DEMO
     while True:
         news = ask_for_news()
- 
         broker: Broker = SimulatedBroker()
         # broker.place_trade(trend_catcher)
         trader = RegexTrader()
         trader.trade()
+
 
 if __name__ == "__main__":
     start()
--- trend_catcher/trader_ABS.py
+++ trend_catcher/trader_ABS.py
@@ -16,20 +16,16 @@ def _trade_request():
 
 class Trader(ABC):
     @abstractmethod
-    def analyze_news(
-        self, news: MarketNew
-    ):  # takes info regarding the news and analyzes it
+    def analyze_news(self, news: MarketNew):  # takes info regarding the news and analyzes it
         pass
 
     @abstractmethod
-    def trade(
-        self, news: MarketNew
-    ):  # takes news; analyzes it and decides a trade to place
+    def trade(self, news: MarketNew):  # takes news; analyzes it and decides a trade to place
         pass
 
 
 class RegexTrader(Trader):
-    def __init__(self, bullish_patterns: list = None, bearish_patterns: list = None, confidence_needed = 0.5):
+    def __init__(self, bullish_patterns: list = None, bearish_patterns: list = None, confidence_needed=0.5):
         self.bullish_patterns = [""]  # default bullish patterns
         self.bearish_patterns = [""]  # defauly bearish patterns
         self.confidence_needed_to_trade = confidence_needed
@@ -45,8 +41,8 @@ class RegexTrader(Trader):
 
         pass
 
-    def trade(self, broker:Broker, news: MarketNew):
-        trade_request : _trade_request = analyze_news(news)
-        trade_inf = TradeInfo(symbol=trade_request.symbol, entry_price=trade_request.price) # this conversion isn't nice create new file for trade_requests and trade_info later
+    def trade(self, broker: Broker, news: MarketNew):
+        trade_request: _trade_request = self.analyze_news(news)
+        trade_inf = TradeInfo(symbol=trade_request.symbol, entry_price=trade_request.price)  # this conversion isn't nice create new file for trade_requests and trade_info later
         broker.place_trade(trade_inf)
         logging.info(f"symbol={trade_request.symbol}, entry_price={trade_request.price}")

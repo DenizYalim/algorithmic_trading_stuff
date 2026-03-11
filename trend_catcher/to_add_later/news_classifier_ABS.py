33977a5d5e1090f40808d464d4266a98728b581b
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-03-11 22:29:01 +0100

    idk know what ive changed

--- trend_catcher/to_add_later/news_classifier_ABS.py
+++ trend_catcher/to_add_later/news_classifier_ABS.py
@@ -8,9 +8,6 @@ class News_Classifier(ABC):  # This is pretty useless, i may deprecate this late
 
 
 class REGEX_classifier(News_Classifier):
-    def __init__(self):
-        super().__init__()
-
     def classify(self, title: str, content: str):
         # regex ile haber başlığı ve içeriği üzerinden sınıflandırma yapacak
         pass
